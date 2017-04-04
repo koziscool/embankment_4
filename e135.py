8d026bde8cbc114a3fb15fba04ed603770e283b9
koziscool <kozjob@yahoo.com>
2017-04-03 20:49:36 -0500

    again

--- e135.py
+++ e135.py
@@ -1,29 +1,60 @@
         
 import time
-from math import floor, sqrt
+from math import floor 
 
 range_dict = {}
+primes = [2]
+prime_factors = { 2: {2:1} }
+distinct_factors = { 1: [1], 2: [1, 2] }
+
+def build_primes_and_factors_up_to(n):
+    current_counter = primes[-1] + 1
+    while current_counter <= n:
+        is_prime = True
+        for p in primes:
+            if p*p > current_counter:
+                break
+            if current_counter % p == 0:
+                is_prime = False
+                prime_factors[current_counter] =  prime_factors[current_counter / p].copy()
+                if p in prime_factors[current_counter]:
+                    prime_factors[current_counter][p] += 1
+                else:
+                    prime_factors[current_counter][p] = 1
+
+                df = [1]
+                for p in prime_factors[current_counter]:
+                    new_df = []
+                    for exp in xrange(0, prime_factors[current_counter][p] + 1):
+                        for f in df:
+                            new_df.append( f * p ** exp )
+                    df = new_df
+                distinct_factors[current_counter] = df
+                break
+
+        if is_prime:
+            primes.append( current_counter )
+            prime_factors[current_counter] = { current_counter: 1}
+            distinct_factors[current_counter] = [1, current_counter]
+        current_counter += 1
+
 
 def e135():
-    # for n in xrange(1, 10 ** 4 ):
-    n = 1155
-    for a in xrange(1, int(sqrt(n)) + 1):
-        b = n / float(a)
-        if b == n / a:
+    build_primes_and_factors_up_to( 10 ** 6 )
+    for n in xrange(1, 10 ** 6):
+        for a in distinct_factors[n]:
+            b = n / a
             k = (a + b) / 4.0
             h = (5*a + b) / 4.0
-            if k == floor(k):
-                print h, k
+            if h > 2*k and k == floor(k):
+                # print h, k, a, b, 6*h*k - h*h - 5*k*k
                 if n in range_dict:
                     range_dict[n] += 1
                 else:
                     range_dict[n] = 1
 
-    print range_dict
-    # print range_dict[1155]
-    # one_soln = filter( lambda x:range_dict[x] == 10, range_dict.keys())
-    # print len(one_soln), len(range_dict)
-    return 'not ready'
+    ten_solns = filter( lambda x:range_dict[x] == 10, range_dict.keys())
+    return len(ten_solns)
 
 if __name__ == '__main__':
     start = time.time()
