5cd937b8586547e276704c562077fb8faa295ef3
koziscool <kozjob@yahoo.com>
2017-04-10 13:20:47 -0500

    again

--- e121.py
+++ e121.py
@@ -1,32 +1,22 @@
         
 import time
 import operator
-# import math
 
 def e121():
     NUM_TURNS = 15
     prod = lambda arr: reduce( operator.mul, arr, 1)
-    blue_wins = lambda arr: len(filter( lambda c: c == 'B', arr)) > NUM_TURNS / 2
+    blue_wins = lambda arr: len(filter( lambda c: c == '1', arr)) > NUM_TURNS / 2
     blue_probability = 0
 
     for i in xrange(2 ** NUM_TURNS):
-        result_array = []
-        probability_array = []
-        for bit_index in xrange(NUM_TURNS):
-            if i & 2**bit_index:
-                result_array.append('B')
-                probability_array.append( 1.0/(bit_index +2) )
-            else:
-                result_array.append('R')
-                probability_array.append( (bit_index + 1.0)/(bit_index +2) )
+        b_format = '0' + str(NUM_TURNS) +'b'
+        bin_str = format( i, b_format)
 
-        if blue_wins( result_array ):
-            blue_probability += prod( probability_array )
+        if blue_wins( bin_str ):
+            blue_probability += prod( map( lambda c, k: 1.0/(k +2) if c == '1' else  (k + 1.0)/(k +2), bin_str, xrange(NUM_TURNS) ) )
 
-
-    # print blue_probability
     return int( 1 / blue_probability )
-    # return 'not ready'
+
 
 if __name__ == '__main__':
     start = time.time()
