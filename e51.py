eb45006670fff8489300f63fc1e96cc1d6489645
koziscool <kozjob@yahoo.com>
2017-04-11 06:25:42 -0500

    again

---
+++ e51.py
@@ -0,0 +1,15 @@
+
+import time
+
+
+def e51():
+
+    
+if __name__ == "__main__":
+    start = time.time()
+    print
+    print "Euler 51 solution is:",  e51()
+    end = time.time()
+    print "elapsed time is: %.4f milliseconds" % (1000 * (end - start)),
+
+
--- e95.py
+++ e95.py
@@ -68,8 +68,7 @@ def e95():
 if __name__ == "__main__":
     start = time.time()
     print
-    e95()
-    print "Euler 80 solution is:",  e95()
+    print "Euler 95 solution is:",  e95()
     end = time.time()
     print "elapsed time is: %.4f milliseconds" % (1000 * (end - start)),
 
